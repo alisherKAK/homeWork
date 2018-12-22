5af081be1e5749fb20ca93783ca568f832ce0822
alisherKAK <alisher.alish333@gmail.com>
2018-12-23 00:53:57 +0600

    31.12.18 Home Work

---
+++ ClassPracticeWork/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPracticeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int square = 2;
+            int CountSantimetersInOneMeter = 100;
+            int CountDaysInOneWeek = 7;
+            const int TEN = 10;
+            const int HUNDRED = 100;
+            const int NULL = 0;
+            const int ONE = 1;
+
+            //1
+            double x;
+            int aKoficent = 7, bKoficent = 3, cKoficent = 4;
+            Console.Write("Введите значение x для функций y = 7x^2 - 3x + 4: ");
+            x = double.Parse(Console.ReadLine());
+            double result = aKoficent * Math.Pow(x, square) - bKoficent * x + cKoficent;
+            Console.WriteLine("y = " + result);
+            Console.WriteLine();
+
+            //2
+            double radiusCircle, lenghtCircle, squareCircle;
+            Console.Write("Введите значение радиуса окружности: ");
+            radiusCircle = double.Parse(Console.ReadLine());
+            lenghtCircle = square * Math.PI * radiusCircle;
+            squareCircle = Math.Pow(radiusCircle, square) * Math.PI;
+            Console.WriteLine("Circle lenght: " + lenghtCircle);
+            Console.WriteLine("Square lenght: " + squareCircle);
+            Console.WriteLine();
+
+            //3
+            double santimeter, meter;
+            Console.Write("Введите длину(см): ");
+            santimeter = double.Parse(Console.ReadLine());
+            meter = santimeter / CountSantimetersInOneMeter;
+            Console.WriteLine("Эта дллина в метрах: " + meter);
+            Console.WriteLine();
+
+            //4
+            int countOfDate = 234;
+            int fullWeek = countOfDate / CountDaysInOneWeek;
+            Console.WriteLine(fullWeek + " недель в 234 днях");
+            Console.WriteLine();
+
+            //5
+            int number, countOfDigits = NULL, countOfUnits = NULL, sumOfDigits = NULL, multiplyOfDigits = ONE;
+            Console.Write("Введите любое число: ");
+            number = int.Parse(Console.ReadLine());
+            while (number != NULL)
+            {
+                int digit = number % TEN;
+                number /= TEN;
+                if (digit == ONE)
+                {
+                    countOfUnits++;
+                }
+                sumOfDigits += digit;
+                multiplyOfDigits *= digit;
+                countOfDigits++;
+            }
+            Console.WriteLine("Count of digits: " + countOfDigits);
+            Console.WriteLine("Count of units: " + countOfUnits);
+            Console.WriteLine("Sum of digits: " + sumOfDigits);
+            Console.WriteLine("Multiply of digits: " + multiplyOfDigits);
+            Console.WriteLine();
+
+            //6
+            int fourDigitsNumber, sumOfDigitsOfFoutNumberDigits = NULL, multiplyOfDigitsOfFoutNumberDigits = ONE;
+            Console.Write("Введите четырехзначное число: ");
+            fourDigitsNumber = int.Parse(Console.ReadLine());
+            while (fourDigitsNumber != NULL)
+            {
+                int digit = fourDigitsNumber % TEN;
+                fourDigitsNumber /= TEN;
+                sumOfDigitsOfFoutNumberDigits += digit;
+                multiplyOfDigitsOfFoutNumberDigits *= digit;
+            }
+            Console.WriteLine("Sum of digits: " + sumOfDigitsOfFoutNumberDigits);
+            Console.WriteLine("Multiply of digits: " + multiplyOfDigitsOfFoutNumberDigits);
+            Console.WriteLine();
+
+            //7
+            int finalNumber = 456, searchingNumber;
+            int firstDigit = finalNumber % TEN;
+            finalNumber /= TEN;
+            int secondDigit = finalNumber % TEN;
+            finalNumber /= TEN;
+            int thirdDigit = finalNumber % TEN;
+            finalNumber /= TEN;
+            searchingNumber = secondDigit + firstDigit * TEN + thirdDigit * HUNDRED;
+            Console.WriteLine("Searching Number: " + searchingNumber);
+            Console.WriteLine();
+
+            //8
+
+            //a.
+            Console.WriteLine(!true && !true);
+            Console.WriteLine(!false && !false);
+            Console.WriteLine(!true && !false);
+            Console.WriteLine(!false && !true);
+            Console.WriteLine();
+
+            //b.
+            Console.WriteLine(true || (!true && true));
+            Console.WriteLine(true || (!true && false));
+            Console.WriteLine(false || (!false && false));
+            Console.WriteLine(false || (!false && true));
+            Console.WriteLine();
+
+            //c.
+            Console.WriteLine((!true && true) || true);
+            Console.WriteLine((!false && true) || true);
+            Console.WriteLine((!false && false) || false);
+            Console.WriteLine((!false && true) || true);
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HomeWork31_12_18/HomeWork31_12_18/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork31_12_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NULL = 0;
+            const int ONE = 1;
+            const int TEN = 10;
+            const int TWO = 2;
+            const int CUBE = 3;
+
+            const int maxNumber = 100;
+            const int numberOfMeterPerSantimeter = 100;
+            const int daysCountInWeek = 7;
+
+            //1
+            Random randNumber = new Random();
+            int firstNumber = randNumber.Next(maxNumber), secondNumber = randNumber.Next(maxNumber), thirdNumber = randNumber.Next(maxNumber);
+
+            Console.WriteLine(firstNumber + "  " + secondNumber + "  " + thirdNumber);
+            Console.WriteLine("-----------------------------");
+
+
+            //2
+            Console.WriteLine("5");
+            Console.WriteLine("10");
+            Console.WriteLine("21");
+            Console.WriteLine("-----------------------------");
+
+
+            //3
+            int distanceInSantimeter;
+            double distanceInMeter;
+
+            Console.Write("Set distance in santimeters: ");
+            distanceInSantimeter = int.Parse(Console.ReadLine());
+            distanceInMeter = (double)distanceInSantimeter / numberOfMeterPerSantimeter;
+            Console.WriteLine("In " + distanceInSantimeter + " santimeters " + distanceInMeter + " meters");
+            Console.WriteLine("-----------------------------");
+
+
+            //4
+            int daysCount = 234, fullWeeks;
+            fullWeeks = daysCount / daysCountInWeek;
+            Console.WriteLine("In " + daysCount + " days " + fullWeeks + " full week");
+            Console.WriteLine("-----------------------------");
+
+
+            //5
+            int twoDigitsNumber, sumOfDigits, multiplyOfDigits, firstDigit, secondDigit, countOfUnit = NULL;
+
+            Console.Write("Set tow digit number: ");
+            twoDigitsNumber = int.Parse(Console.ReadLine());
+
+            firstDigit = twoDigitsNumber % TEN;
+            twoDigitsNumber /= TEN;
+            secondDigit = twoDigitsNumber % TEN;
+            sumOfDigits = firstDigit;
+            sumOfDigits += secondDigit;
+            multiplyOfDigits = firstDigit;
+            multiplyOfDigits *= secondDigit;
+
+            if (firstDigit == ONE) countOfUnit++;
+            if (secondDigit == ONE) countOfUnit++;
+
+            Console.WriteLine("Count of digits: 2");
+            Console.WriteLine("Count of units: " + countOfUnit);
+            Console.WriteLine("Sum of digits: " + sumOfDigits);
+            Console.WriteLine("Multipy of digits: " + multiplyOfDigits);
+            Console.WriteLine("-----------------------------");
+
+
+            //6
+            Console.WriteLine(true || false);
+            Console.WriteLine(true && false);
+            Console.WriteLine(false || false);
+            Console.WriteLine("-----------------------------");
+
+
+            //7
+            double circleRadius, sideOfASquare;
+
+            Console.Write("Set circle radius: ");
+            circleRadius = double.Parse(Console.ReadLine());
+            Console.Write("Set side of a square: ");
+            sideOfASquare = double.Parse(Console.ReadLine());
+
+            if ((Math.PI * Math.Pow(circleRadius, TWO)) > (Math.Pow(sideOfASquare, 2)))
+            {
+                Console.WriteLine("Circle square bigger than square area");
+            }
+            else if ((Math.PI * Math.Pow(circleRadius, TWO)) < (Math.Pow(sideOfASquare, 2)))
+            {
+                Console.WriteLine("Square area bigger than circle square");
+            }
+            else
+            {
+                Console.WriteLine("Square area and circle square equal");
+            }
+            Console.WriteLine("-----------------------------");
+
+
+            //8
+            double firstObjectVolume, secondObjectVolume;
+            double firstObjectMass, secondObjectMass;
+
+            Console.Write("Set first object mass: ");
+            firstObjectMass = double.Parse(Console.ReadLine());
+            Console.Write("Set first object volume: ");
+            firstObjectVolume = double.Parse(Console.ReadLine());
+
+            Console.Write("Set second object mass: ");
+            secondObjectMass = double.Parse(Console.ReadLine());
+            Console.Write("Set second object volume: ");
+            secondObjectVolume = double.Parse(Console.ReadLine());
+
+            if ((firstObjectMass / firstObjectVolume) > (secondObjectMass / secondObjectVolume))
+            {
+                Console.WriteLine("First object denser then second object");
+            }
+            else if ((firstObjectMass / firstObjectVolume) < (secondObjectMass / secondObjectVolume))
+            {
+                Console.WriteLine("Second object denser then first object");
+            }
+            else
+            {
+                Console.WriteLine("First object density and second object density equal");
+            }
+            Console.WriteLine("-----------------------------");
+
+
+            //9
+            double firstElectricalCircuitResistance, firstElectricalCircuitVoltage;
+            double secondElectricalCircuitResistance, secondElectricalCircuitVoltage;
+
+            Console.Write("Set first electrical circuit voltage: ");
+            firstElectricalCircuitVoltage = double.Parse(Console.ReadLine());
+            Console.Write("Set first electrical circuit resistance: ");
+            firstElectricalCircuitResistance = double.Parse(Console.ReadLine());
+
+            Console.Write("Set second electrical circuit voltage: ");
+            secondElectricalCircuitVoltage = double.Parse(Console.ReadLine());
+            Console.Write("Set second electrical circuit resistance: ");
+            secondElectricalCircuitResistance = double.Parse(Console.ReadLine());
+
+            if ((firstElectricalCircuitVoltage / firstElectricalCircuitResistance) > (secondElectricalCircuitVoltage / secondElectricalCircuitResistance))
+            {
+                Console.WriteLine("First electrical circuit amperage bigger than second electrical circuit");
+            }
+            else if ((firstElectricalCircuitVoltage / firstElectricalCircuitResistance) < (secondElectricalCircuitVoltage / secondElectricalCircuitResistance))
+            {
+                Console.WriteLine("Second electrical circuit amperage bigger than first electrical circuit");
+            }
+            else
+            {
+                Console.WriteLine("First electrical circuit amperage and second electrical circuit amperage equal");
+            }
+            Console.WriteLine("-----------------------------");
+
+
+            //10
+            int firstBorder = 20, secondBorder = 35;
+
+            //a.
+            for (int i = firstBorder; i <= secondBorder; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            //b.
+            firstBorder = 10;
+            while (true)
+            {
+                Console.Write("Set second border: ");
+                secondBorder = int.Parse(Console.ReadLine());
+                if (secondBorder > firstBorder) break;
+            }
+
+            for (int i = firstBorder; i <= secondBorder; i++)
+            {
+                Console.WriteLine(Math.Pow(i, TWO));
+            }
+            Console.WriteLine();
+
+            //c.
+            secondBorder = 50;
+            while (true)
+            {
+                Console.Write("Set first border: ");
+                firstBorder = int.Parse(Console.ReadLine());
+                if (secondBorder > firstBorder) break;
+            }
+
+            for (int i = firstBorder; i <= secondBorder; i++)
+            {
+                Console.WriteLine(Math.Pow(i, CUBE));
+            }
+            Console.WriteLine();
+
+            //d.while (true)
+            while (true)
+            {
+                Console.Write("Set first border: ");
+                firstBorder = int.Parse(Console.ReadLine());
+                Console.Write("Set second border: ");
+                secondBorder = int.Parse(Console.ReadLine());
+                if (secondBorder > firstBorder) break;
+            }
+
+            for (int i = firstBorder; i <= secondBorder; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
