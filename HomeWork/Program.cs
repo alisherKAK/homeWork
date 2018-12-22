28a185c708a571915434b99628258e5c59eb6ac4
alisherKAK <alisher.alish333@gmail.com>
2018-12-23 00:53:01 +0600

    30.12.18 Home Work

---
+++ HomeWork/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Console.WriteLine("First Name: Alisher");
+            Console.WriteLine("Last Name: Kalabaev");
+            Console.WriteLine();
+            //2
+            const int NULL = 0;
+            int numbersCount, numbersSum = NULL;
+            Console.Write("Set numbers count: ");
+            numbersCount = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < numbersCount; i++)
+            {
+                Console.Write("Set " + (i + 1) + " number: ");
+                numbersSum += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Sum of numbers: " + numbersSum);
+
+            Console.ReadLine();
+        }
+    }
+}
