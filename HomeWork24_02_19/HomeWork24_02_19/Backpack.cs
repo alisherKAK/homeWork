49844db05e3d847dd1f9c5877212f39023accd89
alisherKAK <alisher.alish333@gmail.com>
2019-02-16 02:24:14 +0600

    24.02.19 HomeWork

---
+++ HomeWork24_02_19/HomeWork24_02_19/Backpack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork24_02_19
+{
+    public class Backpack
+    {
+        public string Name { get; set; }
+        public ConsoleColor Color { get; set; }
+        public double Weight { get; set; }
+        public string Material { get; set; }
+        public double Width { get; set; }
+        public double Heigth { get; set; }
+        public double Depth { get; set; }
+    }
+}
---
+++ HomeWork24_02_19/HomeWork24_02_19/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HomeWork24_02_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
